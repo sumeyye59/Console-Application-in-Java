8b6c5d7580b9ebaa5a26b4974571e56bc61612ce
sumeyye59 <63192409+sumeyye59@users.noreply.github.com>
2024-06-05 15:03:44 +0300

    Update Program.cpp

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,102 +1,108 @@
+// Program.cpp içeriği
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
 #include <ctime>
-#include <sstream>
-#include <string>
 using namespace std;
-
-class Insan{
-	private:
-		string tcKimlikNo;
-		string isim;
-		int yas;
-		
-	protected:
-		string DoubletoString(double num){
-			ostringstream str;
-			str<<num;
-			return str.str();
-		}
-
-	public:
-		Insan(string tcKimlikNo, string isim,int yas){
-			this->tcKimlikNo = tcKimlikNo;
-			this->isim = isim;
-			this->yas = yas;
-		}
-		string TCKimlikNo(){
-			return tcKimlikNo;
-		}
-		string toString(){
-			return tcKimlikNo+"\t"+isim+"\t"+DoubletoString(yas);
-		}
+enum MALZEME{
+Ahsap, Aluminyum, Fiberglas, Plastik
 };
-
-class Calisan : public Insan
-{
-	private:
-		double maas;
-		int tecrubeYil;
-		bool emeklimi;
-		
-	public:
-		Calisan(string tcKimlikNo,string isim,int yas,double maas,int tecrubeYil,bool emeklimi) : Insan(tcKimlikNo,isim,yas) {
-			this->maas = maas;
-			this->tecrubeYil = tecrubeYil;
-			this->emeklimi = emeklimi;
-		}
-		double IkramiyeHesapla(){
-			return (tecrubeYil*maas) / 2020;
-		}
-		double KatSayiHesapla(double *donemselMaaslar,int uzunluk){
-			int katSayi=1;
-			
-			for(int ind=0;ind<uzunluk;ind++){
-				katSayi += donemselMaaslar[ind]/30;
-				for(int i=0;i<7;i++){
-					if(katSayi%15 == 3) break;
-					katSayi++;
-				}
-			}
-			return katSayi;
-		}
-		bool mutlumu(){
-			int r = rand() % 100;
-			if(r > 5) return true;
-			else return false;
-		}
-		string toString(){
-			return Insan::toString()+
-			"\nCalisan Bilgileri: "+DoubletoString(maas)+" "+DoubletoString(tecrubeYil);
-		}
-};
-
-class Doktor : public Calisan{
-	private:
-		double gunlukVisitSaati;
-	public:
-		Doktor(string tcKimlikNo,string isim,int yas,double maas,int tecrubeYil,bool emeklimi,double gunlukVisitSaati): 
-		Calisan(tcKimlikNo, isim,yas,maas,tecrubeYil,emeklimi){
-			this->gunlukVisitSaati = gunlukVisitSaati;
-		}
-		double KatSayiHesapla(double *donemselMaaslar,int uzunluk)
-		{
-			return 100*Calisan::KatSayiHesapla(donemselMaaslar, uzunluk);
-		}
-		bool operator==(Doktor& sag){
-			return this->TCKimlikNo() == sag.TCKimlikNo();
-		}
-		string toString(){
-			return Calisan::toString()+"\nDoktor Gunluk Visit Sayisi: "+DoubletoString(gunlukVisitSaati);
-		}
-};
-
-int main(){
-	Doktor *d = new Doktor("5462315613","Ahmet",52,25000,20,false,15);
-	cout<<d->toString()<<endl;
-	delete d;
-	return 0;
+class Kisi{
+private:
+string isim;
+int dogumYili;
+double kilo;
+public:
+Kisi(string isim,int dogumYili,double kilo){
+this->isim = isim;
+this->dogumYili = dogumYili;
+this->kilo = kilo;
 }
-
-
+Kisi(){
+this->isim = " ";
+this->dogumYili = 1880;
+this->kilo = 55;
+}
+string getIsim()const{
+return isim;
+}
+int getDogumYili()const{
+return dogumYili;
+}
+double getKilo()const{
+return kilo;
+}
+void YemekYe(double kalori){
+this->kilo += (kalori/1000); 
+}
+void Kos(double metre){
+this->kilo -= (metre/1000);
+}
+};
+class Arac{
+protected:
+Kisi *surucu;
+double benzinLitre;
+public:
+Arac(string isim,int dogumYili,double kilo,double benzinLitre){
+this->surucu = new Kisi(isim,dogumYili,kilo);
+this->benzinLitre = benzinLitre;
+}
+Arac(){
+this->surucu = new Kisi();
+this->benzinLitre = 60;
+}
+void Sur(double km){
+benzinLitre -= ((km/100)*7);
+surucu->Kos(km/100);
+}
+~Arac(){
+delete surucu;
+}
+};
+class Tekne : public Arac{
+private:
+MALZEME malzeme;
+public:
+Tekne(Kisi* kaptan,double depo,MALZEME malzeme):Arac(kaptan->getIsim(),kaptan->getDogumYili(),kaptan->getKilo(), depo)
+{
+this->malzeme = malzeme;
+}
+Tekne():Arac(){
+surucu = new Kisi();
+benzinLitre = 80;
+malzeme = Ahsap;
+}
+string MalzemeToString(){
+switch(malzeme){
+case Ahsap:
+return "Ahsap";
+case Aluminyum:
+return "Aluminyum";
+case Fiberglas:
+return "Fiberglas";
+case Plastik:
+return "Plastik";
+default:
+return "";
+}
+}
+friend ostream& operator<<(ostream& ekran,Tekne& sag){
+ekran<<"Malzeme: "<<sag.MalzemeToString()<<endl;
+ekran<<"Depo: "<<sag.benzinLitre<<endl;
+ekran<<"Kaptan: "<<sag.surucu->getIsim()<<endl;
+return ekran;
+}
+};
+int main(){
+Tekne **tekneler = new Tekne*[3];
+tekneler[0] = new Tekne();
+Kisi *ahmetKaptan = new Kisi("Ahmet",1974,85);
+Kisi *mehmetKaptan = new Kisi("Mehmet",1965,81);
+tekneler[1] = new Tekne(ahmetKaptan,75,Fiberglas);
+tekneler[2] = new Tekne(mehmetKaptan,80,Aluminyum);
+cout<<*tekneler[1]<<endl;
+for(int i=0;i<3;i++) delete tekneler[i];
+delete [] tekneler;
+return 0;
+}
